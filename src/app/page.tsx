21d3078d5b5c4ff18354b772a36ce095b927900e
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-09-18 00:53:09 +0530

    Add graphQL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,30 @@ import Navbar from "@/components/Navbar";
 import ProductCard from "@/components/ProductCard";
 
 export default async function ProductsPage() {
-    const res = await fetch("http://localhost:5001/products"); // Next.js API route
-    const products = await res.json();
+    const query = `
+    query {
+      products {
+        id
+        name
+        price
+        stockCount
+      }
+    }
+  `;
+
+    const res = await fetch("http://localhost:4000/graphql", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query }),
+        cache: "no-store", // disable caching in Next.js server components
+    });
+    const { data } = await res.json();
 
     return (
         <>
             <Navbar />
             <main className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                {products.map((p: any) => (
+                {data.products.map((p: any) => (
                     <ProductCard key={p.id} product={p} />
                 ))}
             </main>
