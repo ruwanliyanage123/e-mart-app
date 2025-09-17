fa773d86e29a15ff38cf28fa9a50ea8033e06b18
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-09-17 23:36:24 +0530

    Add modification

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,6 @@ export default function Navbar() {
         <nav className="bg-blue-600 text-white p-4 flex justify-between">
             <Link href="/" className="font-bold text-xl">E-Mart</Link>
             <div className="space-x-4">
-                <Link href="/products">Products</Link>
                 <Link href="/cart">Cart</Link>
             </div>
         </nav>
